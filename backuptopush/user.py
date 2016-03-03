d32e29eb4868aa41ca3edf227b2d29a692c1b9b5
Amministratore <amministratore@amministratore-System-Name.(none)>
2016-03-03 09:22:21 +0100

    Fixes

--- backuptopush/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-  def __init__(self, uclass, ip, mac):
-    #class for this user (e.g. "Free", "Premium" or "Ultra")
-    self.uclass = uclass
-    #MAC address of user's machine
-    self.mac = mac
-    #IP address of user's machine
-    self.ip = ip
